b2e284a23f67dcd62d7bdb525d808752ba78b812
Pedro Henrique Guedes Silveira <pedrohgsilveira@gmail.com>
2019-05-28 18:15:51 -0300

    t

--- CustomView/Controllers/ProgressCircleViewController.swift
+++ CustomView/Controllers/ProgressCircleViewController.swift
@@ -10,35 +10,62 @@ import UIKit
 
 class ProgressCircleViewController: UITableViewController {
 
-    @IBOutlet weak var progressCircleView: ProgressCircleView?
+    
+    @IBOutlet var progressCircle: [ProgressCircleView]!
     @IBOutlet weak var widthSlider: UISlider!
     @IBOutlet weak var progressSlider: UISlider!
     @IBOutlet weak var targetSlider: UISlider!
+    @IBOutlet weak var progressPercent: UILabel!
+    
+    
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        for i in progressCircle {
+            i.circleWidth = CGFloat(self.widthSlider.value)
+            i.progressValue = CGFloat(self.progressSlider.value)
+            i.targetValue = CGFloat(self.targetSlider.value)
+        }
         
-        self.progressCircleView?.circleWidth = CGFloat(self.widthSlider.value)
-        self.progressCircleView?.progressValue = CGFloat(self.progressSlider.value)
-        self.progressCircleView?.targetValue = CGFloat(self.targetSlider.value)
+        
     }
-    
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
         
-        self.progressCircleView?.setNeedsDisplay()
+        super.viewWillTransition(to: size, with: coordinator)
+        for i in progressCircle{
+            i.setNeedsDisplay()
+            
+        }
     }
-
     @IBAction func widthChange(_ sender: Any) {
-        self.progressCircleView?.circleWidth = CGFloat(self.widthSlider.value)
+        
+        for i in progressCircle{
+            i.circleWidth = CGFloat(self.widthSlider.value)
+        }
     }
-    
     @IBAction func progessChange(_ sender: Any) {
-        self.progressCircleView?.progressValue = CGFloat(self.progressSlider.value)
+        
+        for i in progressCircle{
+            i.progressValue = CGFloat(self.progressSlider.value)
+        }
+        progressPercent.text = String(format: "%.02f %", self.progressSlider.value * 100) + "%"
+
     }
-    
     @IBAction func targetChange(_ sender: Any) {
-        self.progressCircleView?.targetValue = CGFloat(self.targetSlider.value)
+        
+        for i in progressCircle{
+            i.targetValue = CGFloat(self.targetSlider.value)
+        }
+    }
+    @IBAction func ChangeProgressColor(_ sender: UIButton) {
+        for i in progressCircle{
+            i.progressColor = sender.backgroundColor!
+        }
     }
 }
 
